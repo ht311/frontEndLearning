90e3fa9fb1e683a868b6e5b00365d5a3e73e7a8f
im-tky-hrs <takuya.hirose@infomart.co.jp>
2023-10-05 14:47:06 +0900

    sb途中まで

--- nextjs-gihyo-book/src/pages/_docment.tsx
+++ nextjs-gihyo-book/src/pages/_docment.tsx
@@ -1,33 +1,34 @@
-import Document, { DocumentContext, DocumentInitialProps } from "next/document";
-import { ServerStyleSheet } from "styled-components";
+import Document, { DocumentContext, DocumentInitialProps } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 // styled-componentsをSSRでも有効にする
 export default class MyDocment extends Document {
-    static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
-        const sheet = new ServerStyleSheet()
-        const originalRenderPage = ctx.renderPage
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
 
-        try {
-            ctx.renderPage = () =>
-                originalRenderPage({
-                    enhanceApp: (App) => (props) =>
-                        sheet.collectStyles(<App {...props} />),
-                })
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
 
-            const initalProps = await Document.getInitialProps(ctx)
+      const initalProps = await Document.getInitialProps(ctx)
 
-            return {
-                ...initalProps,
-                styles: [
-                    <>
-                        {initalProps.styles}
-                        {sheet.getStyleElement()}
-                    </>
-                ]
-            }
-
-        } finally {
-            sheet.seal()
-        }
+      return {
+        ...initalProps,
+        styles: [
+          <>
+            {initalProps.styles}
+            {sheet.getStyleElement()}
+          </>,
+        ],
+      }
+    } finally {
+      sheet.seal()
     }
+  }
 }
