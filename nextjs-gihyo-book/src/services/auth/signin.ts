bf0732419a45e91df2fa2034dea32c7ed843a477
ht311 <thirosejfe@gmail.com>
2023-10-08 13:06:50 +0900

    リスト6.2まで

---
+++ nextjs-gihyo-book/src/services/auth/signin.ts
@@ -0,0 +1,28 @@
+import { ApiContext,User } from "@/types/data";
+import { fetcher } from "@/utils";
+
+export type SigninParams ={
+    username:string
+    password:string
+}
+
+/**
+ * 認証API
+ * 
+ * 
+ */
+const signin 
+    = async (context:ApiContext, signinParams:SigninParams) : Promise<User> => {
+        return await fetcher(
+            `${context.apiRootUrl.replace(/\/$/g,'')}/auth/signin`,
+            {
+                method:'POST',
+                headers:{
+                    Accept:'application/json',
+                    'content-type':'application/json'
+                },
+                body:JSON.stringify(signinParams)
+            })
+    }
+
+export default signin
