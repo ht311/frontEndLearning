94d1d2ad1f3adbb7ffc1bf73b7ec5af9f8881ab7
ht311 <136913329+ht311@users.noreply.github.com>
2023-10-25 04:42:57 +0900

    Private (#11)
    * リスト6.2まで
    
    * format
    
    * 6.2 end
    
    * add
    
    * eslintを通した
    
    * add
    
    * add
    
    * add
    
    * add
    
    * レッスン2
    
    * レッスン3
    
    * レッスン4
    
    * レッスン5
    
    * end
    
    * AddTodoList
    
    ---------
    
    Co-authored-by: 廣瀬 拓也 <139525640+im-tky-hrs@users.noreply.github.com>

--- nextjs-blog/src/pages/404.tsx
+++ nextjs-blog/src/pages/404.tsx
@@ -1,11 +1,5 @@
-import Layout from "../../components/layout"
-
-export const Custom404 = ():JSX.Element => {
-    return (
-        <Layout>
-            <h1>404 - Page Not Found</h1>
-        </Layout>           
-        )
+export const Custom404 = (): JSX.Element => {
+    return <h1>404 - Page Not Found</h1>
 }
 
 export default Custom404
---
+++ nextjs-blog/src/pages/todo/index.tsx
@@ -0,0 +1,59 @@
+import { NextPage } from "next";
+import InputLabel from "../../../components/atom/input-label";
+import Button from "../../../components/atom/button";
+import { useState } from "react";
+
+export const ToDo: NextPage<any> = () => {
+
+    const [inputValue, setInputValue] = useState("")
+    const [toDoList, setToDoList] = useState<string[]>([])
+
+    // 追加ボタンに紐づくinputの状態管理
+    const inputLabelOnChenge = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value)
+    }
+
+    // 追加ボタン押下
+    const inputButtonClick = () => {
+        toDoList.push(inputValue)
+        setInputValue("")
+    }
+
+    // 削除ボタン押下
+    const deleteButtonClick = (index: number) => {
+        const deleteList = [...toDoList]
+        deleteList.splice(index, 1)
+        setToDoList(deleteList)
+    }
+
+    return (
+        <>
+            <h1>ToDo リスト</h1>
+
+            <div>
+                <InputLabel value={inputValue} onChange={inputLabelOnChenge}/>
+                <Button onClick={inputButtonClick}>
+                    追加
+                </Button>
+            </div>
+
+            <table>
+                {toDoList.map((toDo, index) => (
+                    <tr key={index}>
+                        <td>No{index+1}</td>
+                        <td>{toDo}</td>
+                        <td>
+                            <Button
+                                onClick={() => deleteButtonClick(index)}
+                            >
+                                削除
+                            </Button>
+                        </td>
+                    </tr>
+                ))}
+            </table>
+        </>
+    )
+
+}
+export default ToDo
