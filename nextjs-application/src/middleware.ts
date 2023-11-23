45a9adc47748187d5ee80f6392eaf406416f3166
ht311 <136913329+ht311@users.noreply.github.com>
2023-11-24 01:33:03 +0900

    middlewear.tsにwithauth実装

--- nextjs-application/src/middleware.ts
+++ nextjs-application/src/middleware.ts
@@ -1,4 +1,8 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+    secret: process.env.NEXTAUTH_SECRET,
+});
 
 export const config = {
     // login配下以外は、アクセスに認証を必要とする
